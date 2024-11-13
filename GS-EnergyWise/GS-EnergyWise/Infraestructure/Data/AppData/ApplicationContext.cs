a058da8de17ed3d6db81a1755261bfd68c6773ba
Luiz Otávio <luizotaviosilva492@gmail.com>
2024-11-13 19:53:40 -0300

    Criando Application Context

---
+++ GS-EnergyWise/GS-EnergyWise/Infraestructure/Data/AppData/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using GS_EnergyWise.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GS_EnergyWise.Infraestructure.Data.AppData;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<ComunidadeEntity> Comunidades { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options){}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ComunidadeEntity>().ToTable("GS1_Comunidade");
+    }
+}
